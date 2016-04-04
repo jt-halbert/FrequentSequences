43459efb3cae01cb8e347f71dfac2a9f62b971a3
J.T. Halbert <jt@tetraconcepts.com>
2016-04-04 16:06:41 -0400

    working 1,2 frequent sequences

--- src/spade.jl
+++ src/spade.jl
@@ -9,9 +9,7 @@ type SequenceDBRow
     items::Vector{Item} #items in the event
 end
 
-type SequenceDB
-    rows::Vector{SequenceDBRow}
-end
+typealias SequenceDB Vector{SequenceDBRow}
 
 function prepareSampleDB(filename)
     getitems = function(itemstr::AbstractString)
@@ -27,7 +25,7 @@ end
 
 # accumulates all the occurrences of a given atom(item) into a list
 # TODO create a joiner of two id_list dictionaries... then parallelize
-function getidlists(rows::Vector{SequenceDBRow})
+function getidlists(rows::SequenceDB)
     idlists = Dict{Item,Vector{Tuple{Id,Id}}}()
     for row in rows, item in row.items
         idlists[item] = push!(get(idlists,item,[]),(row.sid,row.eid))
@@ -37,7 +35,7 @@ end
 
 function frequent1sequences(sequencedb::SequenceDB,min_sup::Int64)
     # a sequence is frequent if it appears in more than min_sup sequences
-    idlists = getidlists(sequencedb.rows)
+    idlists = getidlists(sequencedb)
     countsids = function(list::Vector{Tuple{Id,Id}})
         length(unique(map(t->t[1],list)))
     end
@@ -45,33 +43,43 @@ function frequent1sequences(sequencedb::SequenceDB,min_sup::Int64)
     filter((k,v)-> v>min_sup, frequencies)
 end
 
-function horizontaldb(sequence_db::SequenceDB)
+function horizontaldb(sequencedb::SequenceDB)
     horizdb = Dict{Id,Vector{Tuple{Item,Id}}}()
-    for row in sequence_db.rows, item in row.items
+    for row in sequencedb, item in row.items
         horizdb[row.sid]=push!(get(horizdb,row.sid,[]), (item,row.eid))
     end
     return horizdb
 end
 
-
-
-
-function find2sequences(ts::Vector{Tuple{Item,Id}})
+function potentialFreq2seqs(ts::Vector{Tuple{Item,Id}},frequentitems::Set{Item})
     s = Set{Sequence}()
     for i=1:length(ts), j=1:length(ts)
         if i!=j
-            if ts[i][1]!=ts[j][1] # exclude same item
-                if ts[i][2] <= ts[j][2] # only consider subsequent-in-time pairs
-                    if ts[i][2]==ts[j][2]  # same event
-                        # N.B. we assume lexographical order on items within event
-                        push!(s,Vector[sort([ts[i][1],ts[j][1]])])
-                    else
-                        push!(s,Vector[[ts[i][1]],[ts[j][1]]])
+            if in(ts[i][1],frequentitems) && in(ts[j][1],frequentitems)
+                if ts[i][1]!=ts[j][1] # exclude same item
+                    if ts[i][2] <= ts[j][2] # only consider subsequent-in-time pairs
+                        if ts[i][2]==ts[j][2]  # same event
+                            # N.B. we assume lexographical order on items within event
+                            push!(s,Vector[sort([ts[i][1],ts[j][1]])])
+                        else
+                            push!(s,Vector[[ts[i][1]],[ts[j][1]]])
+                        end
                     end
                 end
             end
         end
     end
     return s
+end
+
+function frequent2sequences(sequencedb::SequenceDB, frequentitems::Set{Item} , min_sup::Int64=1)
+    horizdb = horizontaldb(sequencedb)
+    frequencies = Dict{Sequence,Int64}()
+    for (sid,ts) in horizdb
+        for sequence in potentialFreq2seqs(ts,frequentitems)
+            frequencies[sequence]=get(frequencies,sequence,0)+1
+        end
+    end
+    filter((k,v)-> v>min_sup, frequencies)
 end
 
